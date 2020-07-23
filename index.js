689d59447fb5b9b9b20a5f73a31f1c35ee615dd5
BarcaJames <jamie1k_86@yahoo.com>
2020-07-23 17:55:34 -0500

    fix quick reply

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post("/webhook", (req, res) => {
       let webhook_event = entry.messaging[0];
 
       console.log("------------------------------------------------------");
-      console.log("ENTIRE MESSAE", webhook_event);
+      console.log("ENTIRE MESSAE ", webhook_event);
       console.log("------------------------------------------------------");
 
       await searchids(webhook_event.sender.id);
